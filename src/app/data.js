36919227267ed26a196eabee928c0bbf000a0a14
UMESH GURNANI <138359819+UKG08@users.noreply.github.com>
2024-09-08 13:34:08 +0530

    Add files via upload

---
+++ src/app/data.js
@@ -0,0 +1,98 @@
+/*
+Websites:
+
+- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
+- https://lucide.dev/icons/ (Lucide Icons)
+- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
+- https://skillicons.dev (Skill Icons to show skills)
+- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
+
+:root {
+  --background: 27 27 27;
+  --foreground: 225 225 225;
+  --muted: 115 115 115;
+  --accent: 254 254 91; #FEFE5B
+}
+
+*/
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "EcoTracker",
+    description: "Track your carbon footprint",
+    date: "2022-08-15",
+    demoLink: "https://ecotracker.example.com",
+  },
+  {
+    id: 2,
+    name: "E-commerce",
+    description: "E-Commere platform made by me ",
+    date: "2022-06-20",
+    demoLink: "git@github.com:UKG08/e-commerce.git",
+  },
+  {
+    id: 3,
+    name: "Tic-Tac Toe",
+    description: "Play the game ",
+    date: "2022-09-10",
+    demoLink: "git@github.com:UKG08/tic-tac-toe.git",
+  },
+  {
+    id: 4,
+    name: "Spotify",
+    description: "Spotify for listening music",
+    date: "2022-05-30",
+    demoLink: "https://github.com/UKG08/spootify.git",
+  },
+  {
+    id: 5,
+    name: "Youtube ",
+    description: "Play videos as many possible",
+    date: "2022-07-12",
+    demoLink: "https://github.com/UKG08/Youtube-clone-.git",
+  },
+  {
+    id: 6,
+    name: "Portfolios",
+    description: "Log your travels",
+    date: "2022-10-01",
+    demoLink: "https://journeylogger.example.com",
+  },
+  {
+    id: 7,
+    name: "3D Portfolios ",
+    description: "Collaborative learning platform",
+    date: "2022-04-18",
+    demoLink: "https://studybuddy.example.com",
+  },
+  
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://github.com/UKG08",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/umesh-gurnani-b7878628b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "X",
+    link: "https://www.x.com",
+    icon: "twitter",
+    newTab: true,
+  },
+  
+];
+
+
